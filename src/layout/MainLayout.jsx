8ab919242f977d1a6174bfccf8c1e0b666448d69
Yuange29 <tancat7895@gmail.com>
2026-03-03 16:50:29 +0700

    change name

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import FooterLayout from "../components/footer/FooterLayout";
-import HeaderLayout from "../components/Header/HeaderLayout";
+import HeaderLayout from "../components/header/HeaderLayout";
 
 const Main = styled.main`
   min-height: calc(100vh - 64px - 120px);
